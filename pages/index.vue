67fab9a5187d2bac683a3d71519681129c52de1f
David Castillo <hola@davecastworks.com>
2020-03-26 19:27:19 -0400

    Project base config files

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+
+    <h1 class="text--reey">Asi comenzamos</h1>
+    <i class="dc-icon-facebook"></i>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Inicio",
+  head() {
+    return {
+      title: "David Castillo"
+    };
+  }
+};
+</script>
+
+<style></style>
