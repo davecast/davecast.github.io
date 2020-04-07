7a94f1cd5e763180eb737f8fc8ba5540284a3f2c
David Castillo <hola@davecastworks.com>
2020-04-07 17:25:15 -0400

    Config url for projects web

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,38 +1,54 @@
-const locale = process.env.NUXT_LOCALE || 'es'
+const locale = process.env.NUXT_LOCALE || "es";
+import axios from "axios";
 
 export default {
   head: {
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
-    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.png" }],
     bodyAttrs: {
-      class: ['']
-    }
+      class: [""],
+    },
   },
   css: [
     // CSS file in the project
-    '@/assets/css/style.css',
+    "@/assets/css/style.css",
   ],
   http: {
-    proxy: true
+    proxy: true,
   },
   manifest: {
-    lang: locale
+    lang: locale,
   },
-  loading: { 
-    color: 'blue',
-    height: '3px' 
+  loading: {
+    color: "blue",
+    height: "3px",
   },
   generate: {
     fallback: true,
-    interval: 100
+    interval: 100,
+    routes() {
+      let projectsWeb = axios
+        .get("https://api.davecastworks.com/routes/", { params: { slug: "proyectos_web" } })
+        .then((res) => {
+          return res.data.map((projectWeb) => {
+            return "/proyectos-web/" + projectWeb.postName;
+          });
+        });
+      /*let users = axios.get('https://api.com/users', {params: {size: 10}}).then((res) => {
+        return res.data.content.map((user) => {
+          return '/user/' + user.id
+        })
+      })*/
+      return Promise.all([projectsWeb]).then((values) => {
+        return values.join().split(",");
+      });
+    },
   },
   layoutTransition: {
-    name: 'layout',
-    mode: 'out-in'
-  }
-}
+    name: "layout",
+    mode: "out-in",
+  },
+};
---
+++ pages/proyectos-web/_webs.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="page__content">
+    <div class="h1">Nombre del proyecto: {{ project }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DetailProjectWeb",
+  data () {
+    return {
+      project: this.$route.params.webs
+    }
+  }
+};
+</script>
+
+<style></style>
