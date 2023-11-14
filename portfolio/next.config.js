d676a98f8c7f021558be3779665c0588a1029fc5
David Castillo <davecas26@gmail.com>
2023-11-14 04:21:35 -0400

    [modified] add new token instagram

--- portfolio/next.config.js
+++ portfolio/next.config.js
@@ -2,9 +2,11 @@
 const nextConfig = {
   output: "export",
   trailingSlash: true,
+  images: { unoptimized: true },
+  basePath: "",
   env: {
     instagramToken:
-      "IGQWRNNUJad09INmlHa3N5Q3ViRmJxbk90a2Rqamg2Q3BKZAVdoTXVLU1VGWUszSUphUWY0eUdyY01UN0kzeHhUVmFjclNlR0h1YTUtODJkeUVnMFlidUpfZAjExZAVBsNmpqMmFDZA2VDQ1hBWU5XcV9UNmNfVndoSDAZD",
+      "IGQWROaEdHMkdCZA09xQW1EbEVyT2hrdzE0T0F0X0pIcUM0R3ZAaMHpDbVR2bW5fVkZAieUVKQlRfRVFLMFoxWi1YejAxemlveFBDb1pGYU1UN2dPVFRiRTFlMzJkVTB6Y0NQRlJHQ3ZAweGlzaXlFM1piWTExTk9STmcZD",
   },
 };
 
