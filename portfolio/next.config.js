495028a6492040e6a182b5488c3dc060c2ee0a82
David Castillo <davecas26@gmail.com>
2023-11-14 04:11:45 -0400

    [modified] add build files

--- portfolio/next.config.js
+++ portfolio/next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  trailingSlash: true,
   env: {
     instagramToken:
       "IGQWRNNUJad09INmlHa3N5Q3ViRmJxbk90a2Rqamg2Q3BKZAVdoTXVLU1VGWUszSUphUWY0eUdyY01UN0kzeHhUVmFjclNlR0h1YTUtODJkeUVnMFlidUpfZAjExZAVBsNmpqMmFDZA2VDQ1hBWU5XcV9UNmNfVndoSDAZD",
