ca50e658e855738c0c355eb2465c612e1c5f15e3
David Castillo <davecas26@gmail.com>
2023-11-21 15:45:39 -0400

    [modified] fix type error

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import LayoutClient from "@/app/ui/LayoutClient";
 export const metadata: Metadata = {
   title: "David Castillo | Portfolio",
   description:
-    "Hola me llamo David Castillo soy desarrollador web y diseñador gráfico, tienes alguna idea contactame",
+    "Hola me llamo David Castillo soy desarrollador web y diseñador gráfico, tienes alguna idea contáctame.",
 };
 
 export default function RootLayout({
